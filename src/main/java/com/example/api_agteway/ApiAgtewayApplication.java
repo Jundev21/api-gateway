7bb3a8d37d217f636de2a4b6cb8f61b1ebfa3e22
Jun <jundev21@gmail.com>
2026-09-01 16:20:55 +0900

    first commit

---
+++ src/main/java/com/example/api_agteway/ApiAgtewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_agteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgtewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgtewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_agteway/ApiAgtewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_agteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgtewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
